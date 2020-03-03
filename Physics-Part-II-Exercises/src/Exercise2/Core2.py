a5c012f8264b204e17b91bb1ff3abb1dd1fdc8d1
jsturatc <james.chapman@me.com>
2020-03-03 15:27:13 +0000

    Finished off Core 2 and saved the graphics from it

--- Physics-Part-II-Exercises/src/Exercise2/Core2.py
+++ Physics-Part-II-Exercises/src/Exercise2/Core2.py
@@ -20,6 +20,37 @@ def DampingIncreaseGraph ():
     plt.ylabel("Angle")
     plt.xlabel("Time")
     plt.legend()
+    plt.savefig("Angle-time-Damped.png",bbox_inches='tight')
+    plt.show()
+    
+def DrivingForceIncreaseGraph ():
+    tspan = [0,10*2*pi]
+    yinit=[0,0]
+    q = 0.5
+    for F in [0.5,1.2,1.44,1.465]:
+        f = funInput(q,F)
+        sol = integrate.solve_ivp(f, tspan, yinit, max_step=0.1)
+        angle = sol.y[0]%(2*pi)
+        angle = np.where(angle < pi, angle, angle-2*pi)
+        plt.figure("Angle")
+        plt.plot(sol.t,angle,label=(f"F =  {F}"))
+        plt.figure("Velocity")
+        plt.plot(sol.t,sol.y[1],label=(f"F =  {F}"))
+    
+    plt.figure("Angle")
+    plt.title("Plot showing amplitude vs time for different Driving Forces with q = 0.5")
+    plt.ylabel("Angle")
+    plt.xlabel("Time")
+    plt.legend()
+    plt.savefig("Angle-time-Forced.png",bbox_inches='tight')
+    
+    plt.figure("Velocity")
+    plt.title("Plot showing velocity vs time for different Driving Forces with q = 0.5")
+    plt.ylabel("Velocity")
+    plt.xlabel("Time")
+    plt.legend()
+    plt.savefig("Velocity-Time-Forced.png",bbox_inches='tight')
     plt.show()
 
-DampingIncreaseGraph()
+DampingIncreaseGraph()    
+DrivingForceIncreaseGraph()
