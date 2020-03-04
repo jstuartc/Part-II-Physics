884bc9b284a510af6c15d3d23e6e3bdfaec9b330
jsturatc <james.chapman@me.com>
2020-03-04 12:43:28 +0000

    Filled out CreateCoilX method

--- Physics-Part-II-Exercises/src/Exercise3A/Core1.py
+++ Physics-Part-II-Exercises/src/Exercise3A/Core1.py
@@ -11,6 +11,21 @@ def FindB (Position,Coil):
     
     return B(x,y,z)
 
-def CreateCoilX(Centre,Radius):
-    n = np.line
-    return Coil(array of dl at R)
+def CreateCoilX(r,Radius): #Creates a coil along x axis at r with a radius producing an array of position and direction of segments 
+    dlNumber = 50
+    n = np.linspace(0,2*pi,num = dlNumber)
+    dlLength = 2*pi*Radius/dlNumber
+    coilInfo = np.empty((dlNumber,6)) # First 3 values give position r, last 3 values give dl
+    coilInfo[:,0] = r[0] # set x value
+    coilInfo[:,1] = r[1] + Radius*np.cos(n)
+    coilInfo[:,2] = r[2] + Radius*np.sin(n)
+    coilInfo[:,3] = 0
+    coilInfo[:,4] = dlLength*(-np.sin(n))
+    coilInfo[:,5] = dlLength*(np.cos(n))
+    
+    return coilInfo
+
+Coil000 = CreateCoilX([0,2,2], 1)
+
+plt.plot(Coil000[:,1],Coil000[:,2])
+plt.show()
