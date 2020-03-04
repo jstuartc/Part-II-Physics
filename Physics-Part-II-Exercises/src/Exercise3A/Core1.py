1539a234c1593b5a2843aa9b948b9b7ddcceb117
jsturatc <james.chapman@me.com>
2020-03-04 12:20:36 +0000

    Created Core 1, defined some methods

---
+++ Physics-Part-II-Exercises/src/Exercise3A/Core1.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import pi
+
+def computedB ():
+    mu0 = 4*pi*10**(-7)
+    #dB = mu0/4π * Idl^r/r^3
+    return dB
+
+def FindB (Position,Coil):
+    
+    return B(x,y,z)
+
+def CreateCoilX(Centre,Radius):
+    n = np.line
+    return Coil(array of dl at R)
